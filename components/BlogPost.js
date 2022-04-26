65ee7af03e5eb33da82e884c4be304754e5d6bc7
yym68686 <31803242+yym68686@users.noreply.github.com>
2022-04-26 16:58:36 +0800

    mo

--- components/BlogPost.js
+++ components/BlogPost.js
@@ -8,15 +8,15 @@ const BlogPost = ({ post }) => {
       <a>
         <article key={post.id} className="mb-6 md:mb-8">
           <header className="flex flex-col justify-between md:flex-row md:items-baseline">
-            <h2 className="text-lg md:text-xl font-medium mb-2 cursor-pointer text-black dark:text-gray-100">
+            <h2 className="text-lg md:text-xl font-medium mb-2 cursor-pointer text-black dark:text-gray-200">
               {post.title}
             </h2>
-            <time className="flex-shrink-0 text-gray-600 dark:text-gray-400">
+            <time className="flex-shrink-0 text-gray-600 dark:text-gray-500">
               {formatDate(post?.date?.start_date || post.createdTime, BLOG.lang)}
             </time>
           </header>
           <main>
-            <p className="hidden md:block leading-8 text-gray-700 dark:text-gray-300">
+            <p className="hidden md:block leading-7 text-gray-700 dark:text-gray-400">
               {post.summary}
             </p>
           </main>
